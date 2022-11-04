ffe59b7470c99937d3fc2bb8916cb42296b73b08
Louis Qian <yuq4@illinois.edu>
2022-11-04 15:56:13 -0500

    update visitor pattern

--- jlox/lox/Expr.java
+++ jlox/lox/Expr.java
@@ -3,6 +3,12 @@ package jlox.lox;
 import java.util.List;
 
 abstract class Expr {
+    interface Visitor<R> {
+        R visitBinaryExpr(Binary expr);
+        R visitGroupingExpr(Grouping expr);
+        R visitLiteralExpr(Literal expr);
+        R visitUnaryExpr(Unary expr);
+    }
     static class Binary extends Expr {
         Binary(Expr left, Token operator, Expr right) {
             this.left = left;
@@ -10,6 +16,7 @@ abstract class Expr {
             this.right = right;
         }
 
+        @Override
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitBinaryExpr(this);
         }
@@ -23,6 +30,7 @@ abstract class Expr {
             this.expression = expression;
         }
 
+        @Override
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitGroupingExpr(this);
         }
@@ -34,6 +42,7 @@ abstract class Expr {
             this.value = value;
         }
 
+        @Override
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitLiteralExpr(this);
         }
@@ -46,6 +55,7 @@ abstract class Expr {
             this.right = right;
         }
 
+        @Override
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitUnaryExpr(this);
         }
