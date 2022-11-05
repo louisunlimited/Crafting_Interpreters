c9fc670b89a521e088908208c9daecb9999a07b2
Louis Qian <yuq4@illinois.edu>
2022-11-04 22:56:38 -0500

    feat: statement support | print

--- jlox/lox/Expr.java
+++ jlox/lox/Expr.java
@@ -2,7 +2,7 @@ package jlox.lox;
 
 import java.util.List;
 
-abstract class Expr { // abstract class
+abstract class Expr {
     interface Visitor<R> {
         R visitBinaryExpr(Binary expr);
         R visitGroupingExpr(Grouping expr);
