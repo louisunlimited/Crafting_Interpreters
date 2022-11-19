ad234a2f2df20c73586f7c330279b680c5e3f991
Louis Qian <yuq4@illinois.edu>
2022-11-18 22:27:40 -0600

    clox project setup

---
+++ clox/chunk.h
@@ -0,0 +1,12 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+
+// define code representation.
+// each instruction has a one-byte opcode
+typedef enum {
+    OP_RETURN,
+} OpCode;
+
+#endif
